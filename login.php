f6cfdd4bff59f75e8afcf475ce4b3f43940c322e
Emanuel <emanuel.ricavi@gmail.com>
2022-05-18 16:37:50 -0300

    Banco de Dados

--- login.php
+++ login.php
@@ -1,4 +1,50 @@
 <!DOCTYPE html>
+<?php
+
+//Conexão
+//The require_once expression is identical to require except PHP will check if the file has already been included, and if so, not include (require) it again.
+require_once 'dbconnect.php';
+
+//iniciar a sessão
+session_start();
+
+//se existir o indice btn_entrar , é porque alguem clicou no botão
+if (isset($_POST['btn-entrar'])):
+	//echo "Clicou";
+	$erros = array();
+	//mysqli_escape_string - função que limpa os dados e evita sqlinjection e outros caracteres indevidos.
+	$login = mysqli_escape_string($connect, $_POST['login']);
+	$senha = mysqli_escape_string($connect, $_POST['senha']);
+	
+	
+	if(empty($login) or empty($senha)):
+		$erros[] = "<li> O campo login/senha precisa ser preenchido </li>";
+	else:
+		//criptografando a senha
+		//$senha=md5($senha);
+		//usuario: marta/senha:123456
+		$sql= "SELECT id,login FROM usuarios WHERE login= '$login' AND senha='$senha'";
+		
+		$resultado = mysqli_query($connect,$sql);
+		//fechando a conexão depois de armazenar os dados
+		mysqli_close($connect);
+		
+		//numeros de linhas do resultado da query maior que 0 ou Se houver algum registro na tabela
+		if (mysqli_num_rows($resultado) > 0):
+			$dados=mysqli_fetch_array($resultado);
+			$_SESSION['logado']= true;
+			$_SESSION['id_usuario']= $dados['id'];
+			//comenado que redireciona para página 16home.php - deve criar essa página
+			header('Location: 17home.php');		
+		
+		else:
+			$erros[]="<li>Usuário e senha não conferem.</li>";
+			
+		endif;
+		
+	endif;	
+endif;	
+?>
 <html lang="pt-br">
 
 <head>
@@ -16,7 +62,7 @@
     <div>
       <h1 class="centralizar titulos">Login</h1>
       <div class="d-flex justify-content-center container distancia mt-4">
-        <form action="">
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
           <div class="d-flex gap-4 mt-2">
             <label for="">Email</label>
             <input type="text" class="input" />
@@ -25,7 +71,7 @@
             <label for="">Senha</label>
             <div>
               <input type="text" class="input" />
-              <button type="" class="btn btn-ctt centralizar mt-5">Logar</button>
+              <button type="submit" class="btn btn-ctt centralizar mt-5">Logar</button>
             </div>
           </div>
         </form>
