1e1eec9f0780b1b73a2ed2a8fcf22c0f2525db8d
unknown <viniciuspessanha99@gmail.com>
2022-05-29 09:18:32 -0300

    IMG BICHINHO

--- criarAdocao.php
+++ criarAdocao.php
@@ -12,33 +12,37 @@
 
 
       <div>
-        
-        <button type="file" class="btnAddImg mt-2">Adicionar imagem</button>
-      
-      
+      <img id="imagemPreview" class="btnAddImg mt-2" src="" alt="" onerror='this.style.display = "none"' onload='this.style.display = "block"'>
+
+
       </div>
       <div>
         <form action="" method="POST" enctype="multipart/form-data">
-          
-        
-        <div class="d-flex gap-2">
+
+
+          <div class="d-flex gap-2">
             <label for="">Nome</label>
             <input type="text" class="input" />
           </div>
-        
+
 
 
           <div class="btnvini d-flex gap-2 mt-2">
-          <label for="imagemBicho">Adicionar imagem</label>
-          <input type="file" id="imagemBicho" name="imagemBicho" src="" alt="">
+            <label for="imagemBicho">Adicionar imagem</label>
+            <input onchange="preview()" type="file" id="imagemBicho" name="imagemBicho" src="" alt="">
+            <script>
+              function preview(){
+                imagemPreview.src=URL.createObjectURL(event.target.files[0])
+              }
+            </script>
 
           </div>
-          
-          
+
+
           <div class="d-flex mt-2 gap-2">
             <label for="">Descrição</label>
-            
-            
+
+
             <div class="btn-style">
               <textarea name="" id="" rows="4" class="input"></textarea>
               <button type="submit" name="btn-criar-adocao" class="btn btn-ctt centralizar mt-2">Criar</button>
