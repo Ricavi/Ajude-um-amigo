d816188303e219d84a08f7087efb2021eafd2c42
unknown <viniciuspessanha99@gmail.com>
2022-05-29 15:53:42 -0300

    Banco de dados

--- adocaoCreate.php
+++ adocaoCreate.php
@@ -8,20 +8,22 @@ require_once 'dbconnect.php';
 if (isset($_POST['btn-criar-adocao'])) :
     $erros = array();
 
-    //BANCO DE DADOS
-    $nome = mysqli_escape_string($connect, $_POST['nomeBicho']);
-    $descricao = mysqli_escape_string($connect, $_POST['descricao']);
-    if (isset($_SESSION['logado']) == true) :
 
+    if (isset($_SESSION['logado']) == true) :
+        //BANCO DE DADOS
+        $nome = mysqli_escape_string($connect, $_POST['nomeBicho']);
+        $descricao = mysqli_escape_string($connect, $_POST['descricao']);
 
 
 
         // IMAGEM    
         //FOMARTOS PPERMITIDOS
         $formatosPermitidos = array("jpg", "jpeg", "jpg", "avif");
-
+        //id do usuario
         $idUser = $_SESSION['id_user'];
-        mkdir("imgBichos/$idUser");
+        if (!is_dir("imgBichos/$idUser")) :
+            mkdir("imgBichos/$idUser");
+        endif;
         $extensao = pathinfo($_FILES['imagemBicho']['name'], PATHINFO_EXTENSION);
         if (in_array($extensao, $formatosPermitidos)) :
             $pasta = "imgBichos/";
@@ -36,6 +38,15 @@ if (isset($_POST['btn-criar-adocao'])) :
             endif;
         else :
             $mensagem = "Formato inválido";
+        endif;
+
+        //Armazenar diretorio da imagem no Banco de dados
+
+        $diretorio = "imgBichos/$idUser/$novoNome";
+
+        //Armazena TUdo no banco de dados
+        $sql = "INSERT INTO bichinho(nome, diretorio, descricao, fk_id) VALUES ('$nome','$diretorio','$descricao','$idUser')";
+        if (mysqli_query($connect, $sql)) :
         endif;
         echo $mensagem;
     endif;
