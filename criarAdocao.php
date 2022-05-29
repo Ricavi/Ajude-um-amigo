32fa7eab01995c7a262c2aae384f3267d03bf52a
unknown <viniciuspessanha99@gmail.com>
2022-05-29 08:07:35 -0300

    File input

--- criarAdocao.php
+++ criarAdocao.php
@@ -7,29 +7,49 @@
   <section class="fundo">
     <h1 class="titulo">Criar adoção</h1>
     <div class="d-flex justify-content-around align-items-center container distancia">
+
+
+
+
       <div>
-        <button class="btnAddImg mt-2">Adicionar imagem</button>
+        
+        <button type="file" class="btnAddImg mt-2">Adicionar imagem</button>
+      
+      
       </div>
       <div>
-        <form action="">
-          <div class="d-flex gap-2">
+        <form action="" method="POST" enctype="multipart/form-data">
+          
+        
+        <div class="d-flex gap-2">
             <label for="">Nome</label>
             <input type="text" class="input" />
           </div>
-          <div class="d-flex gap-2 mt-2">
-            <label for="">Quantia</label>
-            <input type="text" class="input" />
+        
+
+
+          <div class="btnvini d-flex gap-2 mt-2">
+          <label for="imagemBicho">Adicionar imagem</label>
+          <input type="file" id="imagemBicho" name="imagemBicho" src="" alt="">
+
           </div>
+          
+          
           <div class="d-flex mt-2 gap-2">
             <label for="">Descrição</label>
+            
+            
             <div class="btn-style">
               <textarea name="" id="" rows="4" class="input"></textarea>
-              <button class="btn btn-ctt centralizar mt-2">Criar</button>
+              <button type="submit" name="btn-criar-adocao" class="btn btn-ctt centralizar mt-2">Criar</button>
             </div>
           </div>
         </form>
       </div>
     </div>
+
+
+
   </section>
 
   <?php require_once "footer.php";    ?>
