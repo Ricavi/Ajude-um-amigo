7d7c6d1859f5ec47e6667496c6cc53c78b2b4b0b
unknown <viniciuspessanha99@gmail.com>
2022-05-30 15:18:21 -0300

    Não Sei o que faço

--- adocoesCriadas.php
+++ adocoesCriadas.php
@@ -1,4 +1,14 @@
-<?php require_once "head.php"; ?>
+<?php require_once "head.php";
+session_start();
+require_once 'dbconnect.php';
+
+
+if (isset($_SESSION['logado']) == true) :
+
+else :
+  header('Location:login');
+endif;
+?>
 <title>Adoções criadas</title>
 </head>
 
@@ -9,24 +19,26 @@
     <div class="container d-flex justify-content-around gap-3 tamanho">
       <div class="card" style="width: 18rem">
         <div class="card-body">
-          <button class="btn adicionar d-flex">
-            <i class="bi bi-plus-lg plus"></i>
-          </button>
+          <a href="criarAdocao.php">
+            <button class="btn adicionar d-flex">
+              <i class="bi bi-plus-lg plus"></i>
+            </button>
+          </a>
         </div>
       </div>
       <?php
       //Abrir a sessão
-      require_once 'dbconnect.php';
-      session_start();
       $idUser = $_SESSION['id_user'];
       $sqlBicho = "SELECT id FROM bichinho WHERE fk_id='$idUser'";
       $dadosBicho = mysqli_fetch_array(mysqli_query($connect, $sqlBicho));
-      $_SESSION['id_bicho']=$dadosBicho;
+      $_SESSION['id_bicho'] = $dadosBicho;
 
 
       $sqlNome = "SELECT nome FROM bichinho WHERE fk_id='$idUser'";
-      $dadosNome = mysqli_fetch_array(mysqli_query($connect, $sqlNome));
-
+      $dadosNome = mysqli_query($connect, $sqlNome);
+      $ArrayNome=mysqli_fetch_row($dadosNome);
+      //Fazer o indice
+      //Fazer um botão para aumentar o indice até o count de cima
 
 
       $sqlDiretorio = "SELECT diretorio FROM bichinho WHERE fk_id='$idUser'";
@@ -36,8 +48,16 @@
       $dadosDescricao = mysqli_fetch_array(mysqli_query($connect, $sqlDescricao));
 
 
-  //CRIAR UM IF PARA VERIFICAR SE EXISTE ESSAS COISAS AI DE CIMA
-  //PARA NÃO APARECER O ERRO
+      //PARA NÃO APARECER O ERRO
+      if (empty($dadosDescricao)) :
+        $dadosDescricao[0] = ("Nenhuma Descrição encontrada");
+      endif;
+      if (empty($ArrayNome)) :
+        $ArrayNome[0] = ("Nenhuma Nome encontrada");
+      endif;
+      if (empty($dadosDiretorio)) :
+        $dadosDiretorio[0] = ("./img/no_image.jpg");
+      endif;
 
       ?>
 
@@ -49,7 +69,7 @@
         <div class="card-body">
           <h5 class="card-title centralizar tituloCard">
             <?php
-            echo "$dadosNome[0]";
+            echo "$ArrayNome[0]";
             ?>
           </h5>
           <p class="card-text overflow text">
@@ -59,16 +79,15 @@
           </p>
           <div class="d-flex justify-content-around">
 
+            <a href="editarAdoacao.php">
+              <button type="submit" name="btn-editar" class="btn btn-editar ">
+                <i class="bi bi-pencil-fill tamanhoIcons"></i>
+              </button>
+            </a>
 
-            <form action="editar.php" method="POST"></form>
-            <button type="submit" name="btn-editar" class="btn btn-editar ">
-              <i class="bi bi-pencil-fill tamanhoIcons"></i>
-            </button>
 
-
-
-            <form action="deletar.php" method="POST" >
-              <button type="submit"  name="btn-delete" class="btn btn-delete">
+            <form action="deletar.php" method="POST">
+              <button type="submit" name="btn-delete" class="btn btn-delete">
                 <i class="bi bi-trash3-fill tamanhoIcons"></i>
               </button>
             </form>
