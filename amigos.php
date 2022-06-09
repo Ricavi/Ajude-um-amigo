23087d51f887ab54cdd7cd3f2209173bad47b47d
Vinicius De Oliveira Rocha Pessanha <20201TPMI0347@cefetes.br>
2022-06-09 18:38:18 -0300

    Correção de Erros

--- amigos.php
+++ amigos.php
@@ -10,43 +10,45 @@ require_once 'dbconnect.php'; ?>
   <?php require_once "navBar.php"; ?>
   <section class="fundo">
     <h1 class="titulo">Amigos</h1>
-    <div class="VAIDARCERTO">
-      <?php
-      $sqlBicho = "SELECT * FROM bichinho";
-      $bichos = mysqli_query($connect, $sqlBicho);
+    <div>
+      <div class="VAIDARCERTO1">
+        <?php
+        $sqlBicho = "SELECT * FROM bichinho";
+        $bichos = mysqli_query($connect, $sqlBicho);
 
 
-      ?>
-      <?php while ($dados = mysqli_fetch_array($bichos)) { ?>
-        <div class="BATATA container d-flex justify-content-around gap-3 tamanho">
-          <div class="card " style="width: 18rem">
-            <!-- 
+        ?>
+        <?php while ($dados = mysqli_fetch_array($bichos)) { ?>
+          <div class="BATATA1 d-flex justify-content-around gap-3">
+            <div class="card " style="width: 18rem">
+              <!-- 
           Imagem
         -->
-            <img src="<?php echo $dados['diretorio']; ?>" class="card-img-top" alt="..." />
-            <div class="card-body btn-style">
-              <!-- 
+              <img src="<?php echo $dados['diretorio']; ?>" class="card-img-top" alt="..." />
+              <div class="card-body btn-style">
+                <!-- 
           Nome do Animal
         -->
-              <h5 class="card-title centralizarCard tituloCard">
-                <?php
-                echo $dados['nome'];
-                ?>
-              </h5>
-              <!-- 
+                <h5 class="card-title centralizarCard tituloCard">
+                  <?php
+                  echo $dados['nome'];
+                  ?>
+                </h5>
+                <!-- 
           Descrição
         -->
-              <p class="card-text overflow text">
-                <?php
-                echo $dados['descricao'];
-                ?>
-              </p>
-              <a href="chat"><button class="btn btn-am">Adotar</button></a>
+                <p class="card-text overflow text">
+                  <?php
+                  echo $dados['descricao'];
+                  ?>
+                </p>
+                <a href="chat"><button class="btn btn-am">Adotar</button></a>
+              </div>
             </div>
           </div>
-        </div>
-      <?php
-      } ?>
+        <?php
+        } ?>
+      </div>
     </div>
   </section>
   <!--Footer Inicio-->
